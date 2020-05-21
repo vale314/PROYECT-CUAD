5743984a59829f8c591c16e76f326a57ccec3a8a
Vale314 <electrovale314@hotmail.com>
2020-05-21 16:51:53 -0500

    9.1

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -6,36 +6,11 @@ import { Routes, RouterModule } from "@angular/router";
 import { PresentationComponent } from "./presentation/presentation.component";
 import { ComponentsComponent } from "./components/components.component";
 import { SectionsComponent } from "./sections/sections.component";
-import { AboutusComponent } from "./examples/aboutus/aboutus.component";
-import { BlogpostComponent } from "./examples/blogpost/blogpost.component";
-import { BlogpostsComponent } from "./examples/blogposts/blogposts.component";
-import { ContactusComponent } from "./examples/contactus/contactus.component";
-import { EcommerceComponent } from "./examples/ecommerce/ecommerce.component";
-import { LandingComponent } from "./examples/landing/landing.component";
-import { LoginComponent } from "./examples/login/login.component";
-import { ProductpageComponent } from "./examples/productpage/productpage.component";
-import { ProfileComponent } from "./examples/profile/profile.component";
-import { RegisterComponent } from "./examples/register/register.component";
-import { NucleoiconsComponent } from "./components/nucleoicons/nucleoicons.component";
-import { PricingComponent } from "./examples/pricing/pricing.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "bitstory", pathMatch: "full" },
   { path: "bitstory", component: PresentationComponent },
-  { path: "components", component: ComponentsComponent },
-  { path: "sections", component: SectionsComponent },
-  { path: "nucleoicons", component: NucleoiconsComponent },
-  { path: "examples/aboutus", component: AboutusComponent },
-  { path: "examples/blogpost", component: BlogpostComponent },
-  { path: "examples/blogposts", component: BlogpostsComponent },
-  { path: "examples/contactus", component: ContactusComponent },
-  { path: "examples/ecommerce", component: EcommerceComponent },
-  { path: "examples/landing", component: LandingComponent },
-  { path: "examples/login", component: LoginComponent },
-  { path: "examples/pricing", component: PricingComponent },
-  { path: "examples/productpage", component: ProductpageComponent },
-  { path: "examples/profile", component: ProfileComponent },
-  { path: "examples/register", component: RegisterComponent },
+  { path: "letsplay", component: ComponentsComponent },
+  { path: "share", component: SectionsComponent },
 ];
 
 @NgModule({
@@ -43,7 +18,7 @@ const routes: Routes = [
     CommonModule,
     BrowserModule,
     RouterModule.forRoot(routes, {
-      useHash: true,
+      useHash: false,
     }),
   ],
   exports: [],
