fce20a870b0bc9d839b83e1ad716fb6f317215b3
Vale314 <electrovale314@hotmail.com>
2020-05-25 14:47:09 -0500

    Deploy

--- server.js
+++ server.js
@@ -5,15 +5,12 @@ const server = require("http").createServer(app);
 
 const path = require("path");
 
-//Serve static assets in production
-if (process.env.NODE_ENV === "production") {
-  // Set static folder
-  app.use(express.static("client/dist"));
+// Set static folder
+app.use(express.static("client/dist"));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
-  );
-}
+app.get("*", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+);
 
 const PORT = process.env.PORT || 9000;
 
