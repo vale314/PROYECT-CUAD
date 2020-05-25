193f28a90876b4d9030fa7ce1e35517c36977585
Vale314 <electrovale314@hotmail.com>
2020-05-25 14:21:19 -0500

    Deploy

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 const express = require("express");
-const connectDB = require("./config/db");
 const app = express();
 
 const server = require("http").createServer(app);
 
 const path = require("path");
 
-process.env.UV_THREADPOOL_SIZE = 64;
+//Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/dist"));
 
-// Connect Database
-connectDB();
-
-// Init Middleware
-app.use(express.json({ extended: false, limit: "5mb" }));
-
-// Define Routes
-app.use("/api/auth", require("./routes/auth"));
-
-// Serve static assets in production
-//if (process.env.NODE_ENV === "production") {
-// Set static folder
-//app.use(express.static("client/build"));
-
-//app.get("*", (req, res) =>
-//  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-//);
-//}
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 9000;
 
